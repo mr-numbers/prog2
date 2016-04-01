26409b878abf081a00db825d60722a4fe4e7b25d
mr-numbers <carl.herkommer@me.com>
2016-04-01 12:43:35 +0200

    Minor tweaks
    output method tweaked

--- f1_002/Main.java
+++ f1_002/Main.java
@@ -7,42 +7,64 @@ public class Main {
 	Scanner keyboard = new Scanner(System.in);
 	ArrayList<Valuable> valuables = new ArrayList<>();
 
-	public String input() {
+	public String stringInput() {
+		String theString = null;
 		boolean invalid = false;
-		String theString = "Invalid";
-		while (invalid) {
+		do {
+			invalid = false;
 			theString = keyboard.nextLine();
 			if (theString.equals(null)) {
 				invalid = true;
+				System.out.println("String is null - invalid");
 			}
 			if (theString.length() == 0) {
 				invalid = true;
+				System.out.println("Empty string - invalid");
 			}
-			if (theString.equalsIgnoreCase("sampleText")) {
-				invalid = true;
-			}
-		}
+		} while (invalid);
+		
 		return theString;
 	}
 
-	public void output(String output) {
-		System.out.println(output);
+	public int intInput() {
+		int i = keyboard.nextInt();
+		keyboard.nextLine();
+		return i;
 	}
 
 	public void menu() {
 		System.out.println("main menu\n\n1. Add valuable\n2. Display all\n3. Quit");
 	}
-
+	
+	public void output(String text){
+		System.out.println(text);
+	}
 	public void run() {
-		menu();
-		valuables.add(new Appliance("Shitty ass TV", 1200, 2));
-		valuables.add(new Appliance("Another shitty ass TV", 2500, 7));
-		valuables.add(new Stock("Astra Zenica", 200, 5.43));
-		valuables.add(new Stock("Telia", 125, 4.2));
-		valuables.add(new Stock("CSS", 2000, 1.27));
-		valuables.add(new Jewellery("Gold necklace", true, 39));
-		valuables.add(new Jewellery("Gold medallion", true, 57));
-		valuables.add(new Jewellery("Brass bracelet", false, 12));
+		int i = 0;
+		while (i < 1) {
+			menu();
+			int choice = keyboard.nextInt();
+			switch (choice) {
+			case 1:
+				createValuable();
+				break;
+			case 2:
+				displayAll();
+				break;
+			case 3:
+				i = 0;
+			}
+		}
+
+	}
+
+	public void createValuable() {
+		// test
+		String hej = stringInput();
+		output(hej);
+	}
+
+	public void displayAll() {
 		for (Valuable v : valuables) {
 			if (v instanceof Appliance) {
 				v.calculateValue();
@@ -55,11 +77,22 @@ public class Main {
 				output("Stock: " + v.toString());
 			}
 		}
+	}
 
+	public void createSampleData() {
+		valuables.add(new Appliance("Shitty ass TV", 1200, 2));
+		valuables.add(new Appliance("Another shitty ass TV", 2500, 7));
+		valuables.add(new Stock("Astra Zenica", 200, 5.43));
+		valuables.add(new Stock("Telia", 125, 4.2));
+		valuables.add(new Stock("CSS", 2000, 1.27));
+		valuables.add(new Jewellery("Gold necklace", true, 39));
+		valuables.add(new Jewellery("Gold medallion", true, 57));
+		valuables.add(new Jewellery("Brass bracelet", false, 12));
 	}
 
 	public static void main(String[] args) {
 		Main program = new Main();
+		program.createSampleData();
 		program.run();
 	}
 }
